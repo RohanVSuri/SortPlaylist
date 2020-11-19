9c62a6c4e222b70b1e64e7c858f8fad18e2f7aea
Rohan.vs <rsuri@irusmail.com>
2020-11-18 21:00:19 -0500

    added config.py template

---
+++ config.py
@@ -0,0 +1,7 @@
+spotify_client_id="SPOTIFY CLIENT ID HERE"
+spotify_client_secret="SPOTIFY CLIENT SECRET HERE"
+
+lastfm_api_key="LASTFM API KEY HERE"
+lastfm_api_secret="LASTFM API SECRET HERE"
+lastfm_username="LASTFM USERNAME HERE"
+lastfm_password="LASTFM PASSWORD HERE"
